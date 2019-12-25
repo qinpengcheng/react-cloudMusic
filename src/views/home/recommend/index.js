95e512d9b05a5ac56577f42adace0cc491e5cba8
秦鹏程 <754530433@qq.com>
2019-12-25 18:43:06 +0800

    添加Banner

--- src/views/home/recommend/index.js
+++ src/views/home/recommend/index.js
@@ -1,12 +1,48 @@
 import React,{Component} from 'react'
+import './index.scss'
+import Banner from '../../../components/banner'
 class Recommend extends Component{
   constructor(...args){
     super(...args)
   }
+
   render() {
     return(
-      <div>
-        推荐
+      <div className='recommend'>
+        <Banner></Banner>
+        <div className='section'>
+          <div className='section-title'>
+            <h4>推荐歌单</h4>
+            <span>更多></span>
+          </div>
+          <div className='section-list'>
+            <div className='list-row'>
+              <div className='list-col'>
+                <div className=''>
+
+                </div>
+              </div>
+              <div className='list-col'>
+
+              </div>
+              <div className='list-col'>
+
+              </div>
+              <div className='list-col'>
+
+              </div>
+              <div className='list-col'>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='section'>
+          <div className='section-title'>
+            <h4>推荐歌单</h4>
+            <span>更多></span>
+          </div>
+        </div>
       </div>
     )
   }
